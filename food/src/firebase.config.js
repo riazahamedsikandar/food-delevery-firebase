0b779aa058d28baee2ccd300dafeead761830b2d
riazahamedsikandar <riazahamedsikandar@gmail.com>
2023-07-05 22:05:40 +0530

    pck

--- food/src/firebase.config.js
+++ food/src/firebase.config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_bxGG0WgOKDejZvER2fTq2xZZjfUs8zs",
-  authDomain: "restaurantapp-c2ed6.firebaseapp.com",
-  databaseURL: "https://restaurantapp-c2ed6-default-rtdb.firebaseio.com",
-  projectId: "restaurantapp-c2ed6",
-  storageBucket: "restaurantapp-c2ed6.appspot.com",
-  messagingSenderId: "174416156605",
-  appId: "1:174416156605:web:2ec169ea4ef3e7bb25e4d4",
+    apiKey: "AIzaSyAWV7SjnD52ImhXU8jX59KxJAu3YXqj0Yc",
+    authDomain: "food-delivery-47a01.firebaseapp.com",
+    projectId: "food-delivery-47a01",
+    storageBucket: "food-delivery-47a01.appspot.com",
+    messagingSenderId: "607483601576",
+    appId: "1:607483601576:web:6711b9f9ebd3adccea989d",
+    measurementId: "G-DWBZP1WTS8"
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
